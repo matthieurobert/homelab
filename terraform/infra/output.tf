3d269bbc330d1d119209e58d03ecf6b40b6a15bb
Matthieu Robert <matthieurobert82@gmail.com>
2025-12-17 20:41:38 +0100

    feat: deploy csi-proxmox on kome

--- terraform/infra/output.tf
+++ terraform/infra/output.tf
@@ -2,3 +2,8 @@ output "kubeconfig" {
   value = module.k0sctl.kubeconfig
   sensitive = true
 }
+
+output "csi-token" {
+  value = proxmox_virtual_environment_user_token.csi.value
+  sensitive = true
+}
